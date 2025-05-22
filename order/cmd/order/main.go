a077594c707d8d1deb21cca20b5b24131344f9e3
Kartik Vadhawana <kartikvadhwana7@gmail.com>
2025-05-21 20:07:18 -0400

    Update main.go

--- order/cmd/order/main.go
+++ order/cmd/order/main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/akhilsharma90/go-graphql-microservice/order"
+	"github.com/kartikvadhawana/go-graphql-microservice/order"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/tinrab/retry"
 )
