61eb6cc9ab0f2b2e064b9a8aa03bcf185bc0e917
Kartik Vadhawana <kartikvadhwana7@gmail.com>
2025-05-21 20:07:46 -0400

    Update client.go

--- order/client.go
+++ order/client.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/akhilsharma90/go-graphql-microservice/order/pb"
+	"github.com/kartikvadhawana/go-graphql-microservice/order/pb"
 	"google.golang.org/grpc"
 )
 
